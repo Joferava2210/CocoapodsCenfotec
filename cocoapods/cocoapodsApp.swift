cda309152d62bfea653df6de833a94b7966ae82f
joferava <jfelipe221095@gmail.com>
2021-03-13 12:17:47 -0600

    Initial Commit

---
+++ cocoapods/cocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocoapodsApp.swift
+//  cocoapods
+//
+//  Created by Felipe Ramirez Vargas on 13/3/21.
+//
+
+import SwiftUI
+
+@main
+struct cocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
